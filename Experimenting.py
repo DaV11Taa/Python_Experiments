f3dd8dce5b0a8ac82b5564eb2c8d4a9b6bfdf5d7
DaV11Taa <109746299+DaV11Taa@users.noreply.github.com>
2023-07-03 18:19:19 +0000

    Small quiz game shablon

--- Experimenting.py
+++ Experimenting.py
@@ -1,3 +1,15 @@
+def quiz():
+    points = 0
+    print('Which operator has higher precedence in the following list?\na) % (Modulus)\nb) & (BitWise AND)\nc) ** (Exponent)\nd) > (Comparison)')
+    answer = input('Chose your answer: a,b,c or d ->\n')
+    if answer.lower() == 'c':
+        points = points + 1
+    return points
+
 if __name__ == '__main__':
-    name = input('Put your name here: ')
-    print(f'Hello, {name}!')
+    # name = input('Put your name here: ')
+    # print(f'Hello, {name}!')
+    if quiz() == 1:
+        print('congrats you win the game!')
+    else:
+        print('loosseeerr!')
